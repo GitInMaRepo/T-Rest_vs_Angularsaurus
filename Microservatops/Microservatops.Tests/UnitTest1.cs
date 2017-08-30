b8a355cdc72e0b1edf8fda524c5a42362fcd8c9c
GitInMaRepo <oliver.s.roth@gmail.com>
2017-08-30 15:18:18 +0200

    Microservatops: Project structure

---
+++ Microservatops/Microservatops.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microservatops.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
