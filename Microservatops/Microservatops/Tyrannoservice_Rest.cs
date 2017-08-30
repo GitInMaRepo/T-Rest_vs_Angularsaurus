8de5fdd67d0cae0169b963fdf39a3e3cb2fd0083
GitInMaRepo <oliver.s.roth@gmail.com>
2017-08-30 16:18:03 +0200

    Microservatops: NancyFx server returns dinos

---
+++ Microservatops/Microservatops/Tyrannoservice_Rest.cs
@@ -0,0 +1,19 @@
+﻿using Nancy;
+
+namespace Microservatops
+{
+    public class Tyrannoservice_Rest : NancyModule
+    {
+        private DinosaurRepository dinos;
+
+        public Tyrannoservice_Rest()
+        {
+            dinos = new DinosaurRepository();
+
+            Get["/knowndinosaurs"] = _ =>
+            {
+                return Response.AsJson(dinos.GetDinosaurs());
+            };
+        }
+    }
+}
