0aa7fada6592b192efafb9b739740cea24783627
GitInMaRepo <oliver.s.roth@gmail.com>
2017-09-04 14:28:00 +0200

    Angularsaurus: Karma&Jasmine works! Only ATDD driven code from now on

--- angularsaurus/src/app/app.component.spec.ts
+++ angularsaurus/src/app/app.component.spec.ts
@@ -1,13 +1,25 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { DinoDetailComponent } from './dino_detail.component';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+
+import { stegoservice } from './services/stegoservice';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        DinoDetailComponent
       ],
+      imports: [
+        BrowserModule,
+        HttpModule
+      ],
+      providers: [stegoservice],
     }).compileComponents();
   }));
 
@@ -20,13 +32,13 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('The Dino List');
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to The Dino List!');
   }));
 });
